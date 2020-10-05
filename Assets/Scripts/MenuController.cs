907756b3304bde64d21c138682023326f55e7c87
Andrew Cunningham <andrewthecunningham@gmail.com>
2020-10-04 22:33:24 -0700

    ???

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject levelSelectMenu;
+    public GameObject nextLevelMenu;
+    public GameObject optionsMenu;
+    public GameObject creditsMenu;
+
+    public GameObject currentMenu;
+
+    public AudioSource clickSoundEffect;
+
+    public void Start()
+    {
+        GoToMainMenu();
+    }
+
+    public void GoToMenu(GameObject menu)
+    {
+        if(currentMenu != null)
+            currentMenu.SetActive(false);
+
+        menu.SetActive(true);
+        currentMenu = menu;
+
+        clickSoundEffect.Play();
+    }
+
+    public void GoToMainMenu()
+    {
+        GoToMenu(mainMenu);
+    }
+
+    public void GoToLevelSelectMenu()
+    {
+        GoToMenu(levelSelectMenu);
+    }
+
+    public void GoToNextLevelMenu()
+    {
+        GoToMenu(nextLevelMenu);
+    }
+
+    public void GoToOptionsMenu()
+    {
+        GoToMenu(optionsMenu);
+    }
+
+    public void GoToCreditsMenu()
+    {
+        GoToMenu(creditsMenu);
+    }
+
+    public void CloseMenu()
+    {
+        currentMenu.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
